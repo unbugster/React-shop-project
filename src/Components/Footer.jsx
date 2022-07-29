881479050f5bb48ac0a1680021469c2bf796b314
unbugster <unbugster@gmail.com>
2022-07-29 16:47:15 +0300

    Add gh-pages

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,9 @@ const Footer = () => {
             <div className="footer-copyright">
                 <div className="container">
                     © {new Date().getFullYear()} Copyright Text
-                    <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                    <a className="grey-text text-lighten-4 right"
+                        href="https://github.com/unbugster/React-shop-project"
+                        target="_blank" rel="noreferrer">Repo</a>
                 </div>
             </div>
         </footer>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
             <div className="nav-wrapper">
                 <a href="!#" className="brand-logo">React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="!#">Repo</a></li>
+                    <li><a href="https://github.com/unbugster/React-shop-project" target="_blank" rel="noreferrer">Repo</a></li>
                 </ul>
             </div>
         </nav>
